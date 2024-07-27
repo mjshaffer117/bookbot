3d33cab2fd697618cf0c6c332bc21b6ab6e1fda1
mjshaffer117 <mjshaffer117@gmail.com>
2024-07-27 00:23:45 -0400

    edit main

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+def main():
+    path = "./github.com/mjshaffer117/bookbot/books/"
+    file = "frankenstein.txt"
+
+    words = get_text(path + file)
+    print(count_words(words))
+
+
+def get_text(file):
+    with open(file) as contents:
+        return contents.read()
+
+
+def count_words(contents):
+    count = len(contents.split())
+    return count
+
+
+
+if __name__ == "__main__":
+    main()
